d24c75eb5d5c7d005e4c323890e44db84c7fdd46
Federico Tedoldi <federico.tedoldi10@gmail.com>
2023-08-16 13:28:36 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./Estilos.module.css"
+
+const App = () => {
+  return (
+    <div className={styles.divStyles} >
+      <h1> Mi tienda by Coderhouse </h1>
+    </div>
+  );
+};
+
+export default App;
